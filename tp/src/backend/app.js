b40459427dcd9c7d8b0a8598101d0146c8cfc4fb
matlock <jfflores90@gmail.com>
2017-03-26 00:23:30 -0300

    actualizacion

--- tp/src/backend/app.js
+++ tp/src/backend/app.js
@@ -2,16 +2,14 @@ const http = require("http");
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
-const server = http.createServer(app);
+import mongoose from 'mongoose'
+import routes from './routes/routes.js'
 
+mongoose.connect('mongodb://localhost/news')
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+app.use(routes);
 
 
-app.get('/hola',(req,res) => {
-	res.json("hola");
-});
-
-
-server.listen(5000);
+app.listen(3001, () => console.log('SERVER RUNNING ON PORT 3001'));

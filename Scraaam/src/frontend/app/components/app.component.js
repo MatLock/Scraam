e92c90eef3ff99d036a45b9e8a71410b19bade41
Damian Eduardo Gonzalia <dgonzalia@gmail.com>
2017-04-14 16:16:14 -0300

    templates

--- Scraaam/src/frontend/app/components/app.component.js
+++ Scraaam/src/frontend/app/components/app.component.js
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-view',
-  template: `<h1>Scraam</h1>
-            <router-outlet></router-outlet>`,
+  templateUrl: 'app.component.html',
 })
 export default class AppComponent {
 }

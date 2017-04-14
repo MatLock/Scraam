7b11105d776f199d491fde0e7fcd2af72d4c960f
matlock <jfflores90@gmail.com>
2017-04-14 17:13:03 -0300

    agregado template de milestones

--- Scraaam/src/frontend/bootstrap.js
+++ Scraaam/src/frontend/bootstrap.js
@@ -7,6 +7,7 @@ import { BrowserModule } from '@angular/platform-browser'
 import { FormsModule } from '@angular/forms'
 import { HttpModule } from '@angular/http'
 import { RouterModule }  from '@angular/router';
+import { ButtonsModule } from 'ngx-bootstrap';
 
 
 import AppComponent from './app/components/app.component'

029e667e69dde528bced97d9c374c89f6008a1fb
matlock <jfflores90@gmail.com>
2017-04-14 17:14:11 -0300

    agregado template de bootstrap

---
+++ Scraaam/src/frontend/app/components/milestone.component.js
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'milestones',
+  inputs: [ 'milestones' ],
+  templateUrl: '../templates/milestones.component.html'
+})
+export default class MilestoneComponent {
+
+}
